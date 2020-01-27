4340a3fdb5d7e5aff2bc481b5b4613f7abe505fe
Glitch (swatv5) <none>
2020-01-27 16:32:50 +0000

    🍡🌮 Updated with Glitch

--- bot.js
+++ bot.js
@@ -3,9 +3,21 @@
  * Version 1.2.0
  * Robert Borghese
  */
-
 const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
+const express = require('express');
+const keepalive = require('express-glitch-keepalive');
 
+const app = express();
+
+app.use(keepalive);
+
+app.get('/', (req, res) => {
+res.json('This bot should be online! Uptimerobot will keep it alive');
+});
+app.get("/", (request, response) => {
+response.sendStatus(200);
+});
+app.listen(process.env.PORT);
 if(!process.send) {
 
 Files.initStandalone();
